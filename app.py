32b52906ebe6d732a29aae07f5b6e1854b94785b
Soham Prajapati <sohamprajapati0206@gmail.com>
2023-08-15 10:32:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,360 @@
+from flask import Flask, render_template, url_for, redirect, flash, session, request, Response
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError, Email, EqualTo
+from flask_bcrypt import Bcrypt
+from datetime import datetime
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
+from flask_mail import Message, Mail
+import random
+import re
+import pickle
+import cv2
+import mediapipe as mp
+import numpy as np
+
+app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+serializer = Serializer(app.config['SECRET_KEY'])
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# -------------------mail configuration-------------------
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USERNAME"] = 'handssignify@gmail.com'
+app.config["MAIL_PASSWORD"] = 'bluewjzqxngfiqrg'
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+mail = Mail(app)
+# --------------------------------------------------------
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# -------------------Database Model-------------------
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+# ----------------------------------------------------
+
+# -------------------Welcome or Home Page-------------------
+
+
+@app.route('/')
+def home():
+    session.clear()
+    return render_template('home.html')
+# ----------------------------------------------------
+
+
+# -------------------Login Page-------------------
+class LoginForm(FlaskForm):
+    username = StringField(label='username', validators=[InputRequired()], render_kw={"placeholder": "Username"})
+    email = StringField(label='email', validators=[InputRequired(), Email()], render_kw={"placeholder": "Email"})
+    password = PasswordField(label='password', validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # Check if the user has registered before showing the login form
+    if 'registered' in session and session['registered']:
+        session.pop('registered', None)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data) and User.query.filter_by(email=form.email.data).first():
+            login_user(user)
+            flash('Login successfully.', category='success')
+            name = form.username.data
+            session['name'] = name
+            session['logged_in'] = True
+            return redirect(url_for('dashboard'))
+        else:
+            flash(f'Login unsuccessful for {form.username.data}.', category='danger')
+    return render_template('login.html', form=form)
+# ----------------------------------------------------
+
+
+# -------------------Dashboard or Logged Page-------------------
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    if 'logged_in' in session and session['logged_in']:
+        name = session.get('name')
+        return render_template('dashboard.html', name=name)
+    return redirect(url_for('login'))
+# ----------------------------------------------------
+
+# -------------------Logged Out Page-------------------
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    session.clear()
+    logout_user()
+    flash('Account Logged out successfully.', category='success')
+    return redirect(url_for('login'))
+# ----------------------------------------------------
+
+# -------------------Register Page-------------------
+
+class RegisterForm(FlaskForm):
+    username = StringField(label='username', validators=[InputRequired()], render_kw={"placeholder": "Username"})
+    email = StringField(label='email', validators=[InputRequired(), Email()], render_kw={"placeholder": "Email"})
+    password = PasswordField(label='password', validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField(label='confirm_password', validators=[InputRequired(), EqualTo('password')], render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            flash('That Username already exists. Please choose a different one.', 'danger')
+            raise ValidationError('That username already exists. Please choose a different one.')
+
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data,email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        # Set a session variable to indicate successful registration
+        session['registered'] = True
+        flash(f'Account Created for {form.username.data} successfully.', category='success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+# ----------------------------------------------------
+
+# -------------------Update or reset Email Page-------------------
+
+
+class ResetMailForm(FlaskForm):
+    username = StringField(label='username', validators=[InputRequired()], render_kw={"placeholder": "Username"})
+    email = StringField(label='email', validators=[InputRequired(), Email()], render_kw={"placeholder": "Old Email"})
+    new_email = StringField(label='new_email', validators=[InputRequired(), Email()], render_kw={"placeholder": "New Email"})
+    password = PasswordField(label='password', validators=[InputRequired()], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login', validators=[InputRequired()])
+
+
+@app.route('/reset_email', methods=['GET', 'POST'])
+@login_required
+def reset_email():
+    form = ResetMailForm()
+    if 'logged_in' in session and session['logged_in']:
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data) and User.query.filter_by(email=form.email.data).first():
+                user.email = form.new_email.data  # Replace old email with new email
+                db.session.commit()
+                flash('Email reset successfully.', category='success')
+                session.clear()
+                return redirect(url_for('login'))
+            else:
+                flash('Invalid email, password, or combination.', category='danger')
+
+        return render_template('reset_email.html', form=form)
+    return redirect(url_for('login'))
+# --------------------------------------------------------------
+
+# -------------------Forgot Password With OTP-------------------
+
+class ResetPasswordForm(FlaskForm):
+    username = StringField(label='username', validators=[InputRequired()], render_kw={"placeholder": "Username"})
+    email = StringField(label='email', validators=[InputRequired(), Email()], render_kw={"placeholder": "Email"})
+    submit = SubmitField('Submit', validators=[InputRequired()])
+
+
+class ForgotPasswordForm(FlaskForm):
+    username = StringField(label='username', validators=[InputRequired()], render_kw={"placeholder": "Username"})
+    email = StringField(label='email', validators=[InputRequired(), Email()], render_kw={"placeholder": "Email"})
+    new_password = PasswordField(label='new_password', validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "New Password"})
+    confirm_password = PasswordField(label='confirm_password', validators=[InputRequired(), EqualTo('new_password')], render_kw={"placeholder": "Confirm Password"})
+    otp = StringField(label='otp', validators=[InputRequired(), Length(min=6, max=6)], render_kw={"placeholder": "Enter OTP"})
+    submit = SubmitField('Submit', validators=[InputRequired()])
+
+
+@staticmethod
+def send_mail(name, email, otp):
+    msg = Message('Reset Email OTP Password',sender='jalsaparty15@gmail.com', recipients=[email])
+    msg.body = "Hii " + name + "," + "\nYour email OTP is :"+str(otp)
+    mail.send(msg)
+
+
+    # Generate your OTP logic here
+def generate_otp():
+    return random.randint(100000, 999999)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    otp = generate_otp()
+    session['otp'] = otp
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and User.query.filter_by(email=form.email.data).first():
+            send_mail(form.username.data, form.email.data, otp)
+            flash('Reset Request Sent. Check your mail.', 'success')
+            return redirect(url_for('forgot_password'))
+        else:
+            flash('Email and username combination is not exist.', 'danger')
+    return render_template('reset_password_request.html', form=form)
+
+
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    form = ForgotPasswordForm()
+    if form.validate_on_submit():
+        otp = request.form['otp']
+        valid = (otp == request.form['otp'])
+
+        if valid:
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and User.query.filter_by(email=form.email.data).first():
+                user.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+                db.session.commit()
+                flash('Password Changed Successfully.', 'success')
+                return redirect(url_for('login'))
+            else:
+                flash('Email and username combination is not exist.', 'danger')
+        else:
+            flash("OTP verification failed.", 'danger')
+    return render_template('forgot_password.html', form=form)
+# ---------------------------------------------------------------
+
+# ------------------------- Update Password ---------------------
+
+class UpdatePasswordForm(FlaskForm):
+    username = StringField(label='username', validators=[InputRequired()], render_kw={"placeholder": "Username"})
+    email = StringField(label='email', validators=[InputRequired(), Email()], render_kw={"placeholder": "Email"})
+    new_password = PasswordField(label='new_password', validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "New Password"})
+    confirm_password = PasswordField(label='confirm_password', validators=[InputRequired(), EqualTo('new_password')], render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField('Submit', validators=[InputRequired()])
+
+
+@app.route('/update_password', methods=['GET', 'POST'])
+@login_required
+def update_password():
+    form = UpdatePasswordForm()
+    if form.validate_on_submit() and 'logged_in' in session and session['logged_in']:
+
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and User.query.filter_by(email=form.email.data).first():
+                user.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+                db.session.commit()
+                flash('Password Changed Successfully.', 'success')
+                session.clear()
+                return redirect(url_for('login'))
+            else:
+                flash("Username and email combination is not exist.", 'danger')
+    return render_template('update_password.html', form=form)
+# -----------------------------  end  ---------------------------
+
+
+# --------------------------- Machine Learning ------------------
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+
+    hands = mp_hands.Hands(static_image_mode=True,min_detection_confidence=0.3)
+
+    labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M',
+                   13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+
+    while True:
+        data_aux = []
+        x_ = []
+        y_ = []
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        H, W, _ = frame.shape
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+            # ... Rest of the hand landmark processing and prediction code ...
+            data_aux = []
+            x_ = []
+            y_ = []
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+
+                x_.append(x)
+                y_.append(y)
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
+
+            x1 = int(min(x_) * W) - 10
+            y1 = int(min(y_) * H) - 10
+
+            x2 = int(max(x_) * W) - 10
+            y2 = int(max(y_) * H) - 10
+
+            try:
+                prediction = model.predict([np.asarray(data_aux)])
+                predicted_character = labels_dict[int(prediction[0])]
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+                cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                               cv2.LINE_AA)
+
+            except Exception as e:
+                   print(e)
+                   # Handle prediction error
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+# -----------------------------  end  ---------------------------
+if __name__ == '__main__':
+    app.run(debug=True)
